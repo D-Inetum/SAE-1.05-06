cd0b13e1c7a668ab98e54e80e28beb02912beb57
Thibault <steever38@orange.fr>
2025-12-11 15:52:18 +0100

    update

---
+++ js/cv.js
@@ -0,0 +1,16 @@
+const inputCv = document.getElementById('cv');
+const labelText = document.getElementById('file-label-text');
+const defaultText = "Ajouter mon CV";
+
+inputCv.addEventListener('change', function(event) {
+    // On vérifie s'il y a un fichier sélectionné
+    if (this.files && this.files.length > 0) {
+        // On récupère le nom du fichier
+        const fileName = this.files[0].name;
+        // On met à jour le texte
+        labelText.textContent = fileName;
+    } else {
+        // Si l'utilisateur annule, on remet le texte d'origine
+        labelText.textContent = defaultText;
+    }
+});
